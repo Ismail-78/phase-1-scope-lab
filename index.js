0e532873170609aa04ec8aa4640a5d321d2b6047
abdirahman <abdirahman.ismail@student.moringaschool.com>
2023-10-07 15:21:37 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName ='bob';
+const leastFavoriteCustomer= "clegon";
+function upperCaseCustomerName () {
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer () {
+    bestCustomer= 'not bob'
+}
+function overWriteBestCustomer () {
+    bestCustomer= 'maybe bob';
+}
+function changeLeastFavoriteCustomer () {
+    return leastFavoriteCustomer= 'Abdi'
+}
